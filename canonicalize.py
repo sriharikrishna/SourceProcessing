e5f6e3e2220d4171047f1b8adffbfad257e160e9
m_utkej <m_utkej@anlext2wls-84>
2007-09-25 11:44:43 -0500

    reapply option parse bit

--- canonicalize.py
+++ canonicalize.py
@@ -4,6 +4,7 @@ canonicalization
 '''
 import sys
 import cPickle as cp
+from optparse import OptionParser
 
 from PyFort.fortContextFile import fortContextFile,fortUnitContextFile
 
@@ -22,13 +23,20 @@ def hook1(self):
 
 def main():
   _verbose   = True
-  if (len(sys.argv)!=2) :
-    print >>sys.stderr, "Error: missing argument!"
-    print >>sys.stderr, "Usage: ", sys.argv[0], " <input file name>"
-    return 1
-  else :
-    try: 
-      fu = fortUnitContextFile(sys.argv[1],hook1)
+  usage = '%prog [options] <input_file>'
+  opt = OptionParser(usage=usage)
+  opt.add_option('-r','--free',dest='free',
+                 help="free format source",
+                 action='store_true',default=False)
+  opt.add_option('-x','--fixed',dest='fixed',
+                 help='fixed format source',
+                 action='store_true',default=True)
+  config, args = opt.parse_args()
+  if len(args) != 1:
+     opt.error("expect input file argument")
+  fn         = args[0]
+  try: 
+      fu = fortUnitContextFile(fn,hook1)
       for f1 in fu:
 	  f1rw = f1.rewrite(canon_lexi).rewrite(decl_lexi)
 	  slcf = open('reslice.dat','w')
@@ -36,26 +44,26 @@ def main():
 	  pp.dump(f1rw.lines[0].ctxt.toplev.slice_undo)
 	  slcf.close()
 	  f1rw.printit()
-    except UserError,e :
-	print >>sys.stderr, "Error: ", e.msg
-	return 1 
-    except ScanError,e : 
-	print >>sys.stderr, "Error: scanner fails at line:", e.aFortLine.line
-	print >>sys.stderr, " tokens scanned ok: ", e.scanned
-	print >>sys.stderr, "    unable to scan: ", e.rest
-	print >>sys.stderr, " This failure is likely due to possibly legal but unconventional Fortran,"
-	print >>sys.stderr, " such as unusual spacing. Please consider modifying your source code."
-	return 1 
-    except ParseError,e : 
+  except UserError,e :
+      print >>sys.stderr, "Error: ", e.msg
+      return 1 
+  except ScanError,e : 
+      print >>sys.stderr, "Error: scanner fails at line:", e.aFortLine.line
+      print >>sys.stderr, " tokens scanned ok: ", e.scanned
+      print >>sys.stderr, "    unable to scan: ", e.rest
+      print >>sys.stderr, " This failure is likely due to possibly legal but unconventional Fortran,"
+      print >>sys.stderr, " such as unusual spacing. Please consider modifying your source code."
+      return 1 
+  except ParseError,e : 
       print >>sys.stderr, "Error: parser fails to assemble tokens in scanned line:", e.scannedLine, " as ", e.target
       return 1 
-    except AsE, e: 
+  except AsE, e: 
       print >>sys.stderr, "Internal Error: parser failed:", e.msg
       return 1 
-    except LAsE, e: 
+  except LAsE, e: 
       print >>sys.stderr, "Internal Error: parser failed:", e.msg
       return 1 
-    return 0
+  return 0
 
 if __name__ == "__main__":
   sys.exit(main())
