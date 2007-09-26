866cd1dff58aa63eba813d279b6a8e8ea8a96f51
m_utkej <m_utkej@utke2.mcs.anl.gov>
2007-09-26 09:04:56 -0500

    need to replicate normal uses

--- PP/xaifpp.py
+++ PP/xaifpp.py
@@ -37,13 +37,17 @@ def val_deriv_m(self,top):
 
 def add_active_module(self,arg):
     'add the active module'
+    print "self is", self
     if (self.name == 'w2f__types'):
       new_stmt = fs.UseStmt('active_module')
       new_stmt.clone_fmt(self)
       arg.attach(self,new_stmt)
+    else:
+      arg.attach(self)  
 
 def xUse(self,arg):
     'add the active module'
+    print "arg is", arg
     if (self.name == 'w2f__types'):
         new_stmt = fs.UseStmt('active_module')
         new_stmt.clone_fmt(self)
