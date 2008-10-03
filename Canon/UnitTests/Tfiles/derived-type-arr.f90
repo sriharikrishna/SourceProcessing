a16b8d3903bca50b339b49010116f2f352858ec2
lyonsam <lyonsam@cuban>
2008-10-02 20:09:17 -0500

    updated test file and it's correct output

--- Canon/UnitTests/Tfiles/derived-type-arr.f90
+++ Canon/UnitTests/Tfiles/derived-type-arr.f90
@@ -6,6 +6,6 @@ subroutine bar(a)
    type(baz), dimension(4) :: it
 
    it(1)%f1 = 3
-   it(1)%fl = 6 + ff(it(2))
+   it(1)%f1 = 6 + ff(it(2))
 end
 
--- Canon/UnitTests/Tfiles/derived-type-arr.ok.f90
+++ Canon/UnitTests/Tfiles/derived-type-arr.ok.f90
@@ -3,11 +3,13 @@ subroutine bar(a)
       integer :: f1
    end type
 
-   type(baz), dimension(4) :: it
-   real :: ad_ctmp0
+   type(baz),dimension(4) :: it
+  real :: ad_ctmp0
+  type(baz) :: ad_ctmp1
 
    it(1)%f1 = 3
-   call ad_s_ff(it(2),ad_ctmp0)
-   it(1)%fl = 6 + ad_ctmp0
+   ad_ctmp1 = it(2)
+   call ad_s_ff(ad_ctmp1,ad_ctmp0)
+   it(1)%f1 = 6 + ad_ctmp0
 end
 
