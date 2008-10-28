6af8028d8e465807936d71d0dab2004a41dc845e
lyonsam <lyonsam@cuban>
2008-10-28 00:14:36 -0500

    constructing the symbol table: changes to stmt2unit.py

--- PyFort/stmt2unit.py
+++ PyFort/stmt2unit.py
@@ -1,15 +1,14 @@
 '''add methods to fort stmts to build units
 '''
 
-#from _Setup import *
-
-import _Setup
+from _Setup import *
 
 from PyUtil.symtab import Symtab,SymtabEntry
 
 import fortStmts     as fs
 import fortExp       as fe
-import sym_entries   as SE
+
+#TODO: handle derived type entries
 
 def typesep(dd,default_dims):
     '''return name and dimensions for a given decl entry
@@ -29,32 +28,73 @@ def default_dims(attrs_list):
             return tuple(a.args)
     return ()
 
-def _typedecl(stmt,curr):
+def _processTypedeclStmt(aTypeDeclStmt,curr):
     'type declaration -- record type in symbol table'
-
-#    print 'stmt = ',stmt
-    unit    = curr.val
-#    print 'unit = ',unit
-    vtype   = (stmt.__class__,stmt.mod)
-    kw_str  = stmt.kw_str
-    lngth   = kw_str == 'character' and (stmt.mod and stmt.mod[0] or 1)
-    dflt_d  = default_dims(stmt.attrs)
-    symtab = unit.symtab
-#    print 'symtab in typedecl = ',symtab
-    for d in stmt.decls:
-        (name,dims)     = typesep(d,dflt_d)
-#        print 'about to lookup name |',name,'| using ',symtab
-        entry = symtab.lookup_name(name)
-#        print 'entry from symtab = ',entry
-        if entry:
-            entry.enter_type(vtype)
+#   print 'stmt2unit._processTypedeclStmt: called for',aTypeDeclStmt 
+    localSymtab = curr.val.symtab
+    newType = (aTypeDeclStmt.__class__,aTypeDeclStmt.mod)
+    newLength = aTypeDeclStmt.kw_str == 'character' and (aTypeDeclStmt.mod and aTypeDeclStmt.mod[0] \
+                                                                            or 1)
+    dflt_d  = default_dims(aTypeDeclStmt.attrs)
+#   print '\nstmt2unit._typedecl: [stmt = "'+str(stmt)+'" (line number',stmt.lineNumber,')',\
+#                                  ',\tstmt.decls = "'+str(stmt.decls)+'"',\
+#                                  ',\tunit =',unit,\
+#                                  ',\tvtype =',vtype,\
+#                                  ',\tsymtab in typedecl =',symtab,']'
+    for aDecl in aTypeDeclStmt.decls:
+        (name,newDimensions) = typesep(aDecl,dflt_d)
+        theSymtabEntry = localSymtab.lookup_name(name)
+        # already in symtab -> check for conflicts, then add new information
+        if theSymtabEntry:
+#           print '\tdecl "'+str(aDecl)+'" already has SymtabEntry '+str(theSymtabEntry.debug(name))
+            #FIXME: at what LEVEL should this new information be added?
+            # choices are 1-local level, or 2-level in which the entry resides
+            theSymtabEntry.enterType(newType)
+            theSymtabEntry.enterDimensions(newDimensions)
+        # no symtab entry -> create one
         else:
-            entry = SE.var(vtype,
-                           dims=dims,
-                           origin='local',
-                           lngth=lngth)
-            symtab.enter_name(name,entry)
-    return stmt
+            newSymtabEntry = SymtabEntry(SymtabEntry.GenericEntryKind,
+                                         type=newType,
+                                         dimensions=newDimensions,
+                                         length=newLength,
+                                         origin='local')
+#           print '\tdecl "'+str(aDecl)+'" NOT already present in symbol table -- adding '+str(newSymtabEntry.debug(name))
+            localSymtab.enter_name(name,newSymtabEntry)
+    return aTypeDeclStmt
+
+#def _processDeclStmt(aDeclStmt,curr):
+#    print 'called _processDeclStmt on',aDeclStmt
+#    return aDeclStmt
+
+def _processDimensionStmt(aDimensionStmt,curr):
+    localSymtab = curr.val.symtab
+    print 'called _processDimensionStmt on "'+str(aDimensionStmt)+'" with localSymtab',localSymtab
+    for anApp in aDimensionStmt.lst:
+        theSymtabEntry = localSymtab.lookup_name(anApp.head)
+        if theSymtabEntry:
+            print '\tvariable "'+anApp.head+'" already present in symbol table as '+theSymtabEntry.debug(anApp.head)
+            theSymtabEntry.enterDimensions(tuple(anApp.args))
+        else:
+            newSymtabEntry = SymtabEntry(SymtabEntry.VariableEntryKind,
+                                         dimensions=tuple(anApp.args),
+                                         origin='local')
+            print '\tvariable "'+anApp.head+'" NOT already present in symbol table -- adding '+newSymtabEntry.debug(anApp.head)
+            localSymtab.enter_name(anApp.head,newSymtabEntry)
+    return aDimensionStmt
+
+def _processExternalStmt(anExternalStmt,curr):
+    localSymtab = curr.val.symtab
+#   print 'stmt2unit._processExternalStmt: called on "'+str(anExternalStmt)+'" with localSymtab',localSymtab
+    for aProcedureName in anExternalStmt.procedureNames:
+        theSymtabEntry = localSymtab.lookup_name(aProcedureName)
+        if not theSymtabEntry:
+            newSymtabEntry = SymtabEntry(SymtabEntry.ProcedureEntryKind,
+                                         origin='external')
+            localSymtab.enter_name(aProcedureName,newSymtabEntry)
+#           print '\tprocedure NOT already present in symbol table -- adding '+newSymtabEntry.debug(aProcedureName)
+#       else:
+#           print '\tprocedure already has SymtabEntry'+theSymtabEntry.debug(aProcedureName)
+    return anExternalStmt
 
 def _assign2stmtfn(s,cur):
     'convert assign stmt s to stmtfn, and enter in unit symtab'
@@ -73,7 +113,7 @@ def _is_stmt_fn(s,cur):
     'determine if assignment s is a statement function, based on "unit" symtab'
 #    print 'checking assignment ',s,' for stmt fn'
     lhs  = s.lhs
-    look = cur.val.symtab.lookup_dims
+    look = cur.val.symtab.lookupDimensions
 
     return isinstance(lhs,fe.App) and isinstance(lhs.head,str) and not look(lhs.head)
 
@@ -88,11 +128,11 @@ def _use_module(s,cur):
 
     return s
 
-def _make_fn_entry(self,lcl):
-    return SE.func(self.name,self.args,lcl,self.ty)
+def _makeFunctionEntry(self,localSymtab):
+    return SymtabEntry(SymtabEntry.FunctionEntryKind)
 
-def _make_subr_entry(self,lcl):
-    return SE.subr(self.name,self.args,lcl)
+def _makeSubroutineEntry(self,localSymtab):
+    return SymtabEntry(SymtabEntry.SubroutineEntryKind)
 
 def _unit_entry(self,cur):
     '''enter a subroutine or function into:
@@ -100,34 +140,31 @@ def _unit_entry(self,cur):
        2. The unit symtab
        3. The parent of the unit (if there is one)
     '''
-    name  = self.name
-    cur_stab = cur.val.symtab
-    entry = self.make_unit_entry(cur_stab)
-    cur_stab.enter_name(name,entry)
-    if cur_stab.parent:
-        cur_stab.parent.enter_name(name,entry)
-
+    currentSymtab = cur.val.symtab
+    entry = self.make_unit_entry(currentSymtab)
+    currentSymtab.enter_name(self.name,entry)
+    if currentSymtab.parent:
+        currentSymtab.parent.enter_name(self.name,entry)
     return self
 
 def _implicit(self,cur):
     '''Set up the implicit table
     '''
-    ctxt = cur.val
-
-#    if ctxt._in_iface:
-#        return line
+    currentUnit = cur.val
+#   if currentUnit._in_iface:
+#       return line
 
     letters = 'abcdefghijklmnopqrstuvwxyz'
 
     for (tval,tlst) in self.lst:
         for exp in tlst:
             if isinstance(exp,str):
-                ctxt.symtab.implicit[exp] = tval
+                currentUnit.symtab.implicit[exp] = tval
             else:
                 for l in letters[ \
                     letters.find(exp.a1) : \
                     letters.find(exp.a2)+1]:
-                    ctxt.symtab.implicit[l] = tval
+                    currentUnit.symtab.implicit[l] = tval
 
     return self
 
@@ -150,39 +187,59 @@ def _end_unit_iface_action(self,cur):
     unit.symtab = unit.symtab.parent
     return self
 
-def _iface(self,cur):
-    ctxt = cur.val
-    ctxt._in_iface = True
+def _beginInterface(self,cur):
+    print 'called _beginInterface on ',self
+    currentUnit = cur.val
+    currentUnit._in_iface = True
+    # make a symbol table local to the interface
+    interfaceSymtab = Symtab(currentUnit.symtab)
+    # for named interfaces, add a symbol table entry to its symtab AND the enclosing unit
+    if self.name:
+        entry = SE.InterfaceEntry(self.name,interfaceSymtab)
+        currentUnit.symtab.enter_name(self.name,entry)
+        interfaceSymtab.enter_name(self.name,entry)
+    # switch to the interface's symbol table for the duration of the interface
+    currentUnit.symtab = interfaceSymtab
+    print '\tEntering interface "'+str(self.name)+'", with symbol table "'+str(interfaceSymtab)+'"'
     return self
 
-def _end_iface(self,cur):
-    ctxt._in_iface = False
+def _endInterface(self,cur):
+    currentUnit = cur.val
+    currentUnit._in_iface = False
+    # swtch back to the symbol table for the unit
+    currentUnit.symtab = currentUnit.symtab.parent
+    print '\tLeaving interface and restoring unit symbol table "'+str(currentUnit.symtab)+'"\n'
     return self
 
 fs.GenStmt.unit_action        = lambda s,*rest,**kw: s
 fs.GenStmt.unit_entry         = lambda s,*rest,**kw: s
 
 fs.SubroutineStmt.unit_entry      = _unit_entry
-fs.SubroutineStmt.make_unit_entry = _make_subr_entry
+fs.SubroutineStmt.make_unit_entry = _makeSubroutineEntry
 fs.SubroutineStmt.unit_action     = _unit_iface_action
 
 fs.FunctionStmt.unit_entry      = _unit_entry
-fs.FunctionStmt.make_unit_entry = _make_fn_entry
+fs.FunctionStmt.make_unit_entry = _makeFunctionEntry
 fs.FunctionStmt.unit_action     = _unit_iface_action
 
 fs.AssignStmt.is_decl         = _is_stmt_fn
 fs.AssignStmt.unit_action     = _assign2stmtfn
 
-fs.TypeDecl.unit_action       = _typedecl
+fs.DimensionStmt.unit_action = _processDimensionStmt
+
+fs.ExternalStmt.unit_action = _processExternalStmt
+
+
+fs.TypeDecl.unit_action       = _processTypedeclStmt
 
 fs.UseStmt.unit_action        = _use_module
 
 fs.ImplicitNone.unit_action   = _implicit_none
 fs.ImplicitStmt.unit_action   = _implicit
 
-fs.InterfaceStmt.unit_action  = _iface
+fs.InterfaceStmt.unit_action  = _beginInterface
 
-fs.EndInterfaceStmt.unit_action = _end_iface
+fs.EndInterfaceStmt.unit_action = _endInterface
 
 '''
 if __name__ == '__main__':
