fcc17b7426b3f8d0ae07d6d5a398ca22fd3e21a0
m_utkej <m_utkej@utke2.site>
2009-04-13 11:35:22 -0500

    fix up logic

--- PP/UnitTests/t_postProcessor.py
+++ PP/UnitTests/t_postProcessor.py
@@ -36,42 +36,54 @@ def compareFiles(assertFunc,originalFileName,RefFileName,free):
         os.remove(testFileName)
     except PostProcessError,e:
         print >>sys.stderr,'\nPost-processing Error on line '+str(e.lineNumber)+':\n',e.msg
-        return 1
+        refFile.close()
+        testFile.close()
+        os.remove(testFileName)
+        raise e
     except UserError,e:
         print >>sys.stderr,"Error: ",e.msg
-        return 1
+        refFile.close()
+        testFile.close()
+        os.remove(testFileName)
+        raise e
+    except AssertionError,e:
+        print >>sys.stderr,"Error: "+str(e)+"; comparing files: "+testFileName+" "+os.path.join("Tfiles",RefFileName)+"\n" 
+        refFile.close()
+        testFile.close()
+        raise e
+
 
 class C1(TestCase):
 
     def test00(self):
-        'post-process empty file'
+        'post-process empty file: empty.f'
         compareFiles(self.assertEquals,'empty.f','empty.post.f',free=False)
 
     def test1(self):
-        'post-process file with declarations which should not change: '
+        'post-process file with declarations which should not change: implicit.f'
         #implicit, save, real, integer, etc
         compareFiles(self.assertEquals, 'implicit.f','implicit.post.f',free=False)
 
     def test2(self):
-        'post-process a file with included modules'
+        'post-process a file with included modules: modules.f'
         #active module should be added
         compareFiles(self.assertEquals, 'modules.f','modules.post.f',free=False)
 
     def test3(self):
-        'test type replacements'
+        'test type replacements: active.f'
         compareFiles(self.assertEquals,'active.f','active.post.f',free=False)
 
     def test4(self):
-        'test inline replacement of __value__ and __deriv__ in declaration statements'
+        'test inline replacement of __value__ and __deriv__ in declaration statements: decl_inline.f'
         compareFiles(self.assertEquals,'decl_inline.f','decl_inline.post.f',free=False)
 
     def test5(self):
-        'test inline replacement of __value__ and __deriv__ in execution statements'
+        'test inline replacement of __value__ and __deriv__ in execution statements: exec_inline.f'
         compareFiles(self.assertEquals,'exec_inline.f','exec_inline.post.f',free=False)
 
 
     def test6(self):
-        'test inline replacement of nested __value__ and __deriv__ in decl and execution statements'
+        'test inline replacement of nested __value__ and __deriv__ in decl and execution statements: nested_inline.f'
         compareFiles(self.assertEquals,'nested_inline.f','nested_inline.post.f',free=False)
 
 suite = asuite(C1)
