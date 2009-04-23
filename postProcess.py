d733af1966b842280ec50214ca4ba025b0c50e14
m_utkej <m_utkej@utke2.site>
2009-04-23 09:49:15 -0500

    reorder options, cleanup output files on failure so we don't confuse make

--- postProcess.py
+++ postProcess.py
@@ -22,7 +22,15 @@ import PyFort.fortStmts as fs
 
 from PP.unitPostProcess import UnitPostProcessor,PostProcessError
 from PP.templateExpansion import TemplateExpansion
- 
+
+def cleanup(outFileNameList):
+    for outFile in outFileNameList:
+        if os.path.exists(outFile):
+            try: 
+                os.remove(outFile)
+            except:
+                print >>sys.stderr,'ERROR: cleanup - cannot remove output file '+outFile
+
 def main():
     usage = '%prog [options] <input_file>'
     modes={'f':'forward','r':'reverse'}
@@ -35,33 +43,31 @@ def main():
                    help='appends %d to deriv types instead of removing __deriv__',
                    action='store_true',
                    default=False)
-    opt.add_option('--free',
-                   dest='free',
-                   help="<input_file> is in free format",
-                   action='store_true',
-                   default=False)
-    opt.add_option('-m','--mode',dest='mode',
-                   type='choice', choices=modeChoices,
-                   help='set default options for transformation mode with MODE being one of: '+ modeChoicesHelp+ ' (default is \'f\')',
-                   default='f')
-    opt.add_option('-n',
-                   dest='width',
-                   help='write one compile unit per output file with WIDTH digits prepended to the extension of <input_file>, e.g. for -n 2 and three compile units in an input file named \'a.f\' we create \'a.00.f\', a.01.f\', \'a.02.f\'; cannot be specified together with -o')
-    opt.add_option('-o',
-                   '--output',
-                   dest='output',
-                   help='redirect output to  file OUTPUT (default output is stdout); cannot be specified together with -n',
-                   default=None)
     opt.add_option('-i',
                    '--inline',
                    dest='inline',
                    help='file with definitions for inlinable routines for reverse mode post processing (defaults to ad_inline.f); requires reverse mode ( -m r )',
                    default=None) # cannot set default here because of required reverse mode
+    opt.add_option('-m','--mode',dest='mode',
+                   type='choice', choices=modeChoices,
+                   help='set default options for transformation mode with MODE being one of: '+ modeChoicesHelp+ ' (default is \'f\')',
+                   default='f')
+    opt.add_option('-o',
+                   '--output',
+                   dest='output',
+                   help='redirect output to  file OUTPUT (default output is stdout); cannot be specified together with --width',
+                   default=None)
     opt.add_option('-t',
                    '--template',
                    dest='template',
                    help='file with subroutine template for reverse mode post processing (defaults to ad_template.f) for subroutines that do not have a template file specified via the template pragma; requires reverse mode ( -m r )',
                    default=None) # cannot set default here because of required reverse mode
+    opt.add_option('-v',
+                   '--verbose',
+                   dest='verbose',
+                   help='verbose output to stdout',
+                   action='store_true',
+                   default=False)
     opt.add_option('--abstractType',
                    dest='abstractType',
                    help='change the abstract active type name to be replaced  (see also --concreteType ) to ABSTRACTTYPE; defaults to \'oadactive\')',
@@ -70,19 +76,24 @@ def main():
                    dest='concreteType',
                    help='replace abstract active string (see also --abstractType ) with concrete active type CONCRETETYPE; defaults to \'active\'',
                    default='active')
-    opt.add_option('-v',
-                   '--verbose',
-                   dest='verbose',
-                   help='verbose output to stdout',
+    opt.add_option('--free',
+                   dest='free',
+                   help="<input_file> is in free format",
                    action='store_true',
                    default=False)
-    opt.add_option('-w',
-                   '--whitespace',
+    opt.add_option('--noCleanup',
+                   dest='noCleanup',
+                   help='do not remove the output file(s) if an error was encountered (defaults to False)',
+                   action='store_true',
+                   default=False)
+    opt.add_option('--width',
+                   dest='width',
+                   help='write one compile unit per output file with WIDTH digits prepended to the extension of <input_file>, e.g. for -n 2 and three compile units in an input file named \'a.f\' we create \'a.00.f\', a.01.f\', \'a.02.f\'; cannot be specified together with -o')
+    opt.add_option('--whitespace',
                    dest='whitespace',
                    help='inserts whitespaces between tokens',
                    action='store_true',
                    default=False)
-
 
     config, args = opt.parse_args()
 
@@ -92,7 +103,7 @@ def main():
     inputFile = args[0]
 
     if (config.width and config.output):
-        opt.error("cannot specify both -n and -o.")
+        opt.error("cannot specify both --width and -o.")
 
     # set symtab type defaults
     Symtab.setTypeDefaults((fs.RealStmt,[]),(fs.IntegerStmt,[]))
@@ -138,14 +149,16 @@ def main():
     # set whitespace
     fe.Ops.setWhitespace(config.whitespace)
 
+    outFileNameList=[]
     try:
         if splitUnits:
-            (base,ext) = os.path.splitext(outfile)
+            (base,ext) = os.path.splitext(inputFile)
             unitNumExt = "%0"+str(unitNameWidth)+"d"
             unit_num = 0
             for aUnit in fortUnitIterator(inputFile,config.free):
                 output = base + unitNumExt % unit_num + ext
                 out = open(output,'w')
+                outFileNameList.append(output)
                 UnitPostProcessor(aUnit).processUnit().printit(out)
                 out.close()
                 unit_num += 1
@@ -153,6 +166,7 @@ def main():
             out=None
             if config.output: 
                 out = open(config.output,'w')
+                outFileNameList.append(config.output)
             else:
                 out=sys.stdout
             for aUnit in fortUnitIterator(inputFile,config.free):
@@ -164,17 +178,18 @@ def main():
         if (e.lineNumber>0) :
             sys.stderr.write(' on line '+str(e.lineNumber))
         sys.stderr.write(': '+e.msg+'\n')
-        if config.output: 
-            os.remove(config.output)
+        cleanup(outFileNameList)
         return 1
 
     except SymtabError,e:
         debugstr = e.entry and e.entry.debug('unknown') \
                             or ''
         print >>sys.stderr,'\nERROR: SymtabError on line '+str(e.lineNumber)+':\n',e.msg,'\nfor entry',debugstr
+        cleanup(outFileNameList)
         return 1
     except UserError,e:
         print >>sys.stderr,'\nERROR: UserError:',e.msg
+        cleanup(outFileNameList)
         return 1 
     except ScanError,e: 
         print >>sys.stderr,'\nERROR: ScanError: scanner fails at line '+str(e.lineNumber)+':'
@@ -185,17 +200,21 @@ def main():
         print >>sys.stderr,''
         print >>sys.stderr,"This failure is likely due to possibly legal but unconventional Fortran,"
         print >>sys.stderr,"such as unusual spacing. Please consider modifying your source code."
+        cleanup(outFileNameList)
         return 1 
     except ParseError,e: 
         print >>sys.stderr,'\nERROR: ParseError: parser fails to assemble tokens in scanned line '+str(e.lineNumber)+':'
         print >>sys.stderr,e.scannedLine
         print >>sys.stderr,"as",e.target
+        cleanup(outFileNameList)
         return 1 
     except AssemblerException,e:
         print >>sys.stderr,"\nERROR: AssemblerError: parser failed:",e.msg
+        cleanup(outFileNameList)
         return 1 
     except ListAssemblerException,e:
         print >>sys.stderr,"\nERROR: ListAssemblerError: parser failed:",e.msg
+        cleanup(outFileNameList)
         return 1 
     return 0
 
