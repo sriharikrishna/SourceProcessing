501ebd5ad99018642edcdca572e97ada0e0135dc
m_utkej <m_utkej@utke2.site>
2009-06-18 16:37:40 -0500

    lower case

--- Canon/UnitTests/Tfiles/realConst_withKind.pre.f90
+++ Canon/UnitTests/Tfiles/realConst_withKind.pre.f90
@@ -3,6 +3,6 @@ subroutine foo(v)
   real(w2f__4) ifoo
   real(w2f__8) :: oad_ctmp0
 
-   call oad_s_MAX_r(v,6e0_w2f__8,oad_ctmp0)
+   call oad_s_max_r(v,6e0_w2f__8,oad_ctmp0)
    ifoo = oad_ctmp0
 end
