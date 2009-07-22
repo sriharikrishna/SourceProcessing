9b95b0ffc39d0449167f63eaf6723ce286f11f17
hcm <hcm@serenity>
2009-07-22 06:45:09 -0500

    Fixed rawline issue with spaces before comments after template expansion

--- PP/templateExpansion.py
+++ PP/templateExpansion.py
@@ -95,8 +95,10 @@ class TemplateExpansion(object):
                     re.IGNORECASE)
                 match = pat.search(anExecStmt.rawline)
                 if match:
-                    newStmt = fs.Comments(anExecStmt.rawline[:match.start()])
-                    self.__myNewExecs.append(newStmt)
+                    stmt = anExecStmt.rawline[:match.start()].lstrip()
+                    if len(stmt) != 0:
+                        newStmt = self.__insertSubroutineName(aUnit,fs.Comments(stmt))
+                        self.__myNewExecs.append(newStmt)
                     endline = re.search('[\n]',anExecStmt.rawline[match.end():])
                     if endline:
                         end = match.end()+endline.start()
@@ -112,9 +114,12 @@ class TemplateExpansion(object):
                     # continue template
                     pat = re.compile("[0-9]+")
                     newmatch = pat.search(anExecStmt.rawline[match.end():])
-                    newStmt = fs.Comments(anExecStmt.rawline[match.end()+newmatch.end():])
-                    self.__myNewExecs.append(newStmt)
+                    stmt = anExecStmt.rawline[match.end()+newmatch.end():].strip()
+                    if len(stmt) != 0:
+                        newStmt = self.__insertSubroutineName(aUnit,fs.Comments(stmt))
+                        self.__myNewExecs.append(newStmt)
                 else:
+                    anExecStmt = self.__insertSubroutineName(aUnit,anExecStmt)
                     self.__myNewExecs.append(anExecStmt)
                 continue
             anExecStmt = self.__insertSubroutineName(aUnit,anExecStmt)
