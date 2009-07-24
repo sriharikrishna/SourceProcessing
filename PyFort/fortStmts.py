dcd4685724f8f16ac3d14bd5bac8b92bdbbb5835
hcm <hcm@serenity>
2009-07-24 12:07:22 -0500

    removed whitespace on AssignStmt for consistency with spacing of other statements

--- PyFort/fortStmts.py
+++ PyFort/fortStmts.py
@@ -1169,7 +1169,7 @@ class AssignStmt(Exec):
         return 'AssignStmt(%s,%s)' % (repr(self.lhs),repr(self.rhs))
 
     def __str__(self):
-        return '%s = %s' % (str(self.lhs),str(self.rhs))
+        return '%s=%s' % (str(self.lhs),str(self.rhs))
 
 class PointerAssignStmt(Exec):
     _sons = ['lhs','rhs']
@@ -1193,7 +1193,7 @@ class PointerAssignStmt(Exec):
         return 'PointerAssignStmt(%s,%s)' % (repr(self.lhs),repr(self.rhs))
 
     def __str__(self):
-        return '%s => %s' % (str(self.lhs),str(self.rhs))
+        return '%s=>%s' % (str(self.lhs),str(self.rhs))
 
 class OpenStmt(Exec):
     kw = 'open'
