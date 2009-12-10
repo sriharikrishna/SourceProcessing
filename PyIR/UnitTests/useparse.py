566c244ba9de806800ce6e0d4b88a302e0052c1d
hcm <hcm@serenity>
2009-12-10 13:43:01 -0600

    fixed issue with scanning causing errors in unit tests

--- PyIR/UnitTests/useparse.py
+++ PyIR/UnitTests/useparse.py
@@ -6,13 +6,13 @@ from PyFort.fortStmts import parse
 def scan(s):
     (v,rst) = scan1.scan(s)
     if rst:
-        raise "incomplete scan '%s', rst = '%s'" % (s,rst)
+        raise Exception("incomplete scan '%s', rst = '%s'" % (s,rst))
     return v
 
 def ep(s):
-    (v,rst) = Exp(scan(s))
+    (v,rst) = Exp(filter(lambda x: x != ' ',scan(s)))
     if rst:
-        raise "incomplete Exp parse: '%s', rst = %s" % (s,rst)
+        raise Exception("incomplete Exp parse: '%s', rst = %s" % (s,rst))
     return v
 
 def pps(s):
