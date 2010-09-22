bc8bb2e32206464b129c5b95ceea0e10f8773d00
hcm <hcm@uchicago.edu>
2010-09-22 16:12:48 -0500

    free/fixed format was being set completely incorrectly in the unit testing with a bool instead of a string ('free' or 'fixed). somehow the unit tests were passing despite this... some are now failing as a result of this correction

--- Canon/UnitTests/t_canon.py
+++ Canon/UnitTests/t_canon.py
@@ -19,11 +19,11 @@ DebugManager.setQuiet(True)
 
 Symtab.setTypeDefaults((RealStmt,[]),(IntegerStmt,[]))
 
-def compareFiles(assertFunc,originalFileName,RefFileName,free):
+def compareFiles(assertFunc,originalFileName,RefFileName,format):
     try:
         (fd,testFileName) = tempfile.mkstemp()
         testFile  = open(testFileName,'w')
-        for aUnit in fortUnitIterator(fname_t(originalFileName),free):
+        for aUnit in fortUnitIterator(fname_t(originalFileName),format):
             UnitCanonicalizer(aUnit).canonicalizeUnit().printit(testFile)
         testFile.close()
         testFile = open(testFileName,'r')
@@ -47,81 +47,81 @@ class C1(TestCase):
 
     def test00(self):
         'canonicalize empty file'
-        compareFiles(self.assertEquals,'empty.f','empty.pre.f',free=False)
+        compareFiles(self.assertEquals,'empty.f','empty.pre.f',format='fixed')
 
     def test2(self):
         'canon of max using real consts w embedded kinds'
-        compareFiles(self.assertEquals,'realConst_withKind.f90','realConst_withKind.pre.f90',free=True)
+        compareFiles(self.assertEquals,'realConst_withKind.f90','realConst_withKind.pre.f90',format='free')
 
     def test3(self):
         'canon array of derived types'
-        compareFiles(self.assertEquals,'derived-type-arr.f90','derived-type-arr.ok.f90',free=True)
+        compareFiles(self.assertEquals,'derived-type-arr.f90','derived-type-arr.ok.f90',format='free')
 
     def test5(self):
         'preserve inline comments for statements that have been altered during canonicalization'
-        compareFiles(self.assertEquals,'inlineComment.f90','inlineComment.pre.f90',free=True)
+        compareFiles(self.assertEquals,'inlineComment.f90','inlineComment.pre.f90',format='free')
 
     def test5(self):
         'preserve inline comments for statements that have been altered during canonicalization -- KNOWN TO FAIL (see https://trac.mcs.anl.gov/projects/openAD/ticket/187)'
-        compareFiles(self.assertEquals,'inlineComment.f90','inlineComment.pre.f90',free=True)
+        compareFiles(self.assertEquals,'inlineComment.f90','inlineComment.pre.f90',format='free')
 
     def test6(self):
         'canonicalize a subunit (subroutine contained in program)'
-        compareFiles(self.assertEquals,'canonicalizeSubunit.f90','canonicalizeSubunit.pre.f90',free=True)
+        compareFiles(self.assertEquals,'canonicalizeSubunit.f90','canonicalizeSubunit.pre.f90',format='free')
 
 class C2(TestCase):
     '''Coverage for particular kinds of statements'''
     def test0(self):
         'Hoist function call from if statement (without "then")'
-        compareFiles(self.assertEquals,'ifNonThenStmt.f','ifNonThenStmt.pre.f',free=False)
+        compareFiles(self.assertEquals,'ifNonThenStmt.f','ifNonThenStmt.pre.f',format='fixed')
 
     def test1(self):
         'Hoist function call from if-then statement'
-        compareFiles(self.assertEquals,'ifThenStmt.f','ifThenStmt.pre.f',free=False)
+        compareFiles(self.assertEquals,'ifThenStmt.f','ifThenStmt.pre.f',format='fixed')
 
     def test2(self):
         'Hoist function calls from do and do while statements'
-        compareFiles(self.assertEquals,'doDoWhile.f','doDoWhile.pre.f',free=False)
+        compareFiles(self.assertEquals,'doDoWhile.f','doDoWhile.pre.f',format='fixed')
 
 class TestCanonicalizeSubroutineCall(TestCase):
     '''Subroutine call statements'''
     def test1(self):
         'Hoist intrinsic function call from subroutine call statement'
-        compareFiles(self.assertEquals,'subCall_hoistIntrinsic.f90','subCall_hoistIntrinsic.pre.f90',free=True)
+        compareFiles(self.assertEquals,'subCall_hoistIntrinsic.f90','subCall_hoistIntrinsic.pre.f90',format='free')
 
     def test2(self):
         'Hoist nonintrinsic function call from subroutine call statement'
-        compareFiles(self.assertEquals,'subCall_hoistNonintrinsic.f90','subCall_hoistNonintrinsic.pre.f90',free=True)
+        compareFiles(self.assertEquals,'subCall_hoistNonintrinsic.f90','subCall_hoistNonintrinsic.pre.f90',format='free')
 
     def test2(self):
         'no hoisting simple named parameters'
-        compareFiles(self.assertEquals,'subCall_simpleNamed.f90','subCall_simpleNamed.pre.f90',free=True)
+        compareFiles(self.assertEquals,'subCall_simpleNamed.f90','subCall_simpleNamed.pre.f90',format='free')
 
     def test2(self):
         'hoisting call from named parameter'
-        compareFiles(self.assertEquals,'subCall_namedWithCall.f90','subCall_namedWithCall.pre.f90',free=True)
+        compareFiles(self.assertEquals,'subCall_namedWithCall.f90','subCall_namedWithCall.pre.f90',format='free')
 
 class TestFunctionToSubroutine(TestCase):    
     def test1(self):
         'test converting function to subroutine'
-        compareFiles(self.assertEquals,'funDef2subDef.f','funDef2subDef.pre.f',free=True)
+        compareFiles(self.assertEquals,'funDef2subDef.f','funDef2subDef.pre.f',format='free')
 
     def test2(self):
         'test converting function to subroutine 2'
-        compareFiles(self.assertEquals,'funDef2subDef2.f','funDef2subDef2.pre.f',free=True)
+        compareFiles(self.assertEquals,'funDef2subDef2.f','funDef2subDef2.pre.f',format='free')
 
     def test3(self):
         'test converting function definition and call to subroutine definition and call with function type definition outside function statement'
-        compareFiles(self.assertEquals,'funDef2subDef3.f','funDef2subDef3.pre.f',free=True)
+        compareFiles(self.assertEquals,'funDef2subDef3.f','funDef2subDef3.pre.f',format='free')
 
     def test4(self):
         'test result argument replacement while converting function definition to subroutine definition'
-        compareFiles(self.assertEquals,'funDef_resultReplacement.f90','funDef_resultReplacement.pre.f90',free=True)
+        compareFiles(self.assertEquals,'funDef_resultReplacement.f90','funDef_resultReplacement.pre.f90',format='free')
 
 class TestCanonicalizeEllipsis(TestCase):
     def test1(self):
         'test converting max ellipsis'
-        compareFiles(self.assertEquals,'ellipsisFunCall.f90','ellipsisFunCall.pre.f90',free=True)
+        compareFiles(self.assertEquals,'ellipsisFunCall.f90','ellipsisFunCall.pre.f90',format='free')
         
 suite = asuite(C1,C2,TestCanonicalizeSubroutineCall,TestFunctionToSubroutine,TestCanonicalizeEllipsis)
 
