16dbc85a83d79837451d6c50318945a0966a5e4d
hcm <hcm@uchicago.edu>
2010-10-06 15:08:05 -0500

    set result declaration lead to be the lead of the preceding declaration

--- Canon/function2subroutine.py
+++ Canon/function2subroutine.py
@@ -180,8 +180,8 @@ def convertFunction(functionUnit,newExecs,newDecls):
             newSubUnit.decls.append(aDecl)
 
     if resultDecl is not None:
-        if len(functionUnit.decls) != 0:
-            resultDecl.lead = aDecl.lead
+        if len(newSubUnit.decls) != 0:
+            resultDecl.lead = newSubUnit.decls[-1].lead
         # append declaration for new out parameter
         newSubUnit.decls.append(resultDecl)
         
