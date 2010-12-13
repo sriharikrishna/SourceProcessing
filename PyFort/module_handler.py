6b3dd4950b8c4429c23e9342531eb39b0ce40ef3
utke <utke@linux-l3b2.site>
2010-12-13 16:47:19 -0600

    fix up comments, the whole unit is preserved not just the symbol table

--- PyFort/module_handler.py
+++ PyFort/module_handler.py
@@ -1,4 +1,5 @@
-'''Save and get module symbol tables
+'''
+Save and get module units
 Currently only a thin veneer over caseless dict
 '''
 import _Setup
@@ -6,9 +7,9 @@ from PyUtil.caselessDict import caselessDict as cDict
 
 class ModuleHandler(object):
     def __init__(self):
-        self.modules = cDict()
-    def add_module(self,name,stab):
-        self.modules[name] = stab
+        self.modules = cDict() # for key module name contains as value the module unit 
+    def add_module(self,name,moduleUnit):
+        self.modules[name] = moduleUnit
     def get_module(self,name):
         return self.modules.get(name)
 
