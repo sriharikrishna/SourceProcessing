d2015f91c953eb2b36f3b3604fb28207c8388c02
hcm <hcm@localhost>
2012-01-17 14:58:10 -0600

    update canon unit tests to reflect changes for typetab

--- Canon/UnitTests/Tfiles/realConst_withKind.f90
+++ Canon/UnitTests/Tfiles/realConst_withKind.f90
@@ -1,4 +1,7 @@
       subroutine foo(v)
+        integer :: w2f__4, w2f__8
+        parameter (w2f__4 = kind(0.0))
+        parameter (w2f__8 = kind(0.0d0))
         real(w2f__4) v
         real(w2f__4) ifoo
 
